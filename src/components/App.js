52ee828987c3074a3c43d4eb36246936088c36ae
Ceren Şahin <cerennssahinn@gmail.com>
2023-07-05 13:54:00 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
+import React, { useState, useEffect } from "react";
+
 // create your App component here
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setIsLoaded(true);
+        setImage(data.message);
+      });
+
+  }, []);
+
+  if (isLoaded) {
+    return (
+      <div>
+        <h1>App</h1>
+        <img src={image} alt="A Random Dog" />
+      </div>
+    );
+  }
+
+  else {
+    return <p>Loading...</p>;
+  }
+}
+
+export default App;
